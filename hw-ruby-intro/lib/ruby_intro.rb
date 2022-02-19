78fd5bb70afb1b1f20b7bcc3dc10dbb3cf9edb00
root <grace@uccs.edu>
2022-02-19 04:04:11 +0000

    Section 1 finished

--- hw-ruby-intro/lib/ruby_intro.rb
+++ hw-ruby-intro/lib/ruby_intro.rb
@@ -1,4 +1,4 @@
-# When done, submit this entire file to the autograder
+# When done, submit this entire file to the autograder.
 
 # Part 1
 
@@ -24,26 +24,59 @@ def max_2_sum(array)
   return (sarray[-1]+sarray[-2])
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(array, n)
+  #returns false if the array is empty or has only 1 element
+  return false if array.empty? || array.length==1
+
+  #goes through all permuatation using 2 elments of the array
+  #if any equal n then it returns true
+  array.permutation(2).any? { |a,b| a+b==n}
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  #prints out Hello followed by the entered name
+  return "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant? (s)
+  #check to see if the entered string starts with a consonant
+  /^[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/.match(s)!=nil
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4? (s)
+  #returns true if input is 0
+  return true if s=="0"
+
+  #returns true if the number ends with 00 and contains only 0's and 1's
+  /^[10]*00$/.match(s)!=nil
+
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+  #allows the isbn and price to be accessed
+  attr_accessor :isbn
+  attr_accessor :price
+
+  #constructor
+  def initialize(isbn,price)
+    #gives an error if the input in invalid
+    raise ArgumentError,
+      "Invalid Argument" if isbn.empty? || price<=0
+    #sets the isbn and price entered to the variables
+    @isbn=isbn
+    @price=price
+
+  end
+
+  #converts the price to look nice
+  def price_as_string
+    sprintf("$%2.2f",@price)
+  end
+
 end
